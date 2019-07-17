2a0ce0bcb1112a089ef7f6622d7220432acb58b5
babylee2002 <babylee2002@gmail.com>
2019-07-17 15:21:50 -0400

    hoisting and comparison

--- interview-questions.js
+++ interview-questions.js
@@ -36,6 +36,18 @@ function closure(a){
 //in object, this key word refers to its own object itself.
 //whatever obj it is directly inside of.
 
-//function hoisting/function declaration - function can be called regardless of where the function is located.
-//function expression - expression has to be defined first before the function is called. It is in a way acting as a variable so it has to be defined before getting called.
+
+//hosting
+//variables and functions are hoisted to the top of the scope that they are declared in.
+//variable/function hoisting/function declaration - JS hoist all the variables and functions to the top.
+//function expression functions are set with var or const as a variable.
+//var is a function scope || let and const are block scope
+
+
+//double(==) vs triple(===)  equal comparison operator 
+//double looks at its value 
+//tripe(strict) looks at its value and type
+// number vs string => string converted to number
+//boolean vs non-boolean => non-boolean converted to boolean
+//obj vs primitive data-type => obj is converted to primitive
 

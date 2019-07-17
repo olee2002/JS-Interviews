8a95da417d163614b38dfc73c090afde67b27fa1
babylee2002 <babylee2002@gmail.com>
2019-07-17 14:55:43 -0400

    IIFE & closure

--- interview-questions.js
+++ interview-questions.js
@@ -13,3 +13,29 @@ function tripleAdd1(num1, num2, num3){
 }
 
 tripleAdd(10)(20)(30)
+
+//IIFE (Imediate Invoked Function Expression)
+//preserve any private scope in a function
+//avoid overriding any global/outside scope variables
+
+(function(){
+   console.log("Hello World!")
+})()
+
+//closure
+//a closure in an inner function that has access to the scope of an enclosing function
+//variables inner/outter/ global
+
+function closure(a){
+   return function(){
+    return a
+   }
+}
+
+//this key word
+//in object, this key word refers to its own object itself.
+//whatever obj it is directly inside of.
+
+//function hoisting/function declaration - function can be called regardless of where the function is located.
+//function expression - expression has to be defined first before the function is called. It is in a way acting as a variable so it has to be defined before getting called.
+
